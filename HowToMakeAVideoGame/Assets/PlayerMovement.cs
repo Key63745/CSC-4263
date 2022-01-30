b9d7ad3116333e11be077cc1ac95dceae13d1fc1
Key63745 <jlkey0812@gmail.com>
2022-01-30 17:34:24 -0600

    Project-1
    Prototype for the first individual game assignment.

---
+++ HowToMakeAVideoGame/Assets/PlayerMovement.cs
@@ -0,0 +1,33 @@
+
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //Reference to player rigid body
+    public Rigidbody rb;
+
+    public float fowardForce = 2000f;
+    public float sidewaysForce = 500f; 
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    //Use fixed update when you are manipulating physics
+    void FixedUpdate()
+    {
+        //Add a foward force
+        rb.AddForce(0, 0, fowardForce * Time.deltaTime);
+
+        if(Input.GetKey("d") )
+        {
+            rb.AddForce(500 * Time.deltaTime, 0, 0);
+        }
+
+       if(Input.GetKey("a") )
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
+    }
+}
